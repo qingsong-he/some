9625779030cdaa27de0d9aca6578f7d010c127a3
qingsong-he <qingsong-he@outlook.com>
2021-04-10 21:03:37 +0800

    add log with filepath info

--- dart/base/main.dart
+++ dart/base/main.dart
@@ -2,6 +2,17 @@ import 'dart:isolate';
 import 'dart:mirrors';
 import 'dart:io';
 
+void debugPrint(Object? object) {
+  var outLayer1 = StackTrace.current.toString().split("\n")[1].split(":");
+
+  print([
+    outLayer1[outLayer1.length - 1 - 1 - 1].split(Platform.pathSeparator).last +
+        ":" +
+        outLayer1[outLayer1.length - 1 - 1],
+    object
+  ]);
+}
+
 void main() async {
   // case 1
   {
@@ -29,7 +40,7 @@ void main() async {
 
     const l1 = [1, 2, 3];
     const l2 = [1, 2, 3];
-    print(identical(l1, l2)); // it's true, it's false when use final
+    debugPrint(identical(l1, l2)); // it's true, it's false when use final
 
     num v6;
     v6 = 1;
@@ -43,34 +54,35 @@ void main() async {
     v7 = "hello world";
     v7 = '''hello
   world''';
-    print(v7); // use 'r' prefix diable escape
+    debugPrint(v7); // use 'r' prefix diable escape
 
     String v8 = "hello hello";
-    print(v8.replaceAll("hello", "-"));
+    debugPrint(v8.replaceAll("hello", "-"));
   }
 
   // case 3
   {
     var v9 = DateTime.now();
-    print([v9.timeZoneName, v9.timeZoneOffset, v9.isUtc, v9.hour]);
+    debugPrint([v9.timeZoneName, v9.timeZoneOffset, v9.isUtc, v9.hour]);
 
     var v10 = []; // any elem type
     v10.add("hello");
     v10.add(123);
-    print([v10.first, v10.last, v10.length, v10.isEmpty, v10.reversed, v10]);
+    debugPrint(
+        [v10.first, v10.last, v10.length, v10.isEmpty, v10.reversed, v10]);
 
     // just only one elem type
     var v11 = List.filled(3, 0xff, growable: true);
-    print(v11);
+    debugPrint(v11);
     v11.add(1);
 
     var v12 = Map();
     v12["foo"] = "bar";
     v12[1] = 2;
-    print(v12);
+    debugPrint(v12);
     var v13 = {"foo": "bar"};
     v13["b"] = "b";
-    print([
+    debugPrint([
       v13.isEmpty,
       v13.length,
       v13.keys,
@@ -79,24 +91,24 @@ void main() async {
       v13.containsValue(123)
     ]);
     v13.updateAll((key, value) => "--$value--");
-    print(v13);
+    debugPrint(v13);
     v13.removeWhere((key, value) => key == "foo");
 
     var v14 = Set();
     v14.add("foo");
     v14.add(13);
     v14.addAll([1, 2, 3, "foo", "bar"]);
-    print([v14, v14.isEmpty]);
+    debugPrint([v14, v14.isEmpty]);
   }
 
   // case 4
   {
     String v15 = "😀";
-    print([v15.length, v15.runes.length]);
+    debugPrint([v15.length, v15.runes.length]);
 
     var v16 = Runes("\u{1f596} \u6211");
     var v17 = String.fromCharCodes(v16);
-    print([
+    debugPrint([
       v17,
       v17.codeUnits,
       v17.codeUnitAt(0),
@@ -113,100 +125,100 @@ void main() async {
         .findLibrary(Symbol("dart.core"))
         .declarations
         .forEach((key, value) {
-      print('$key - $value');
+      // debugPrint('$key - $value');
     });
 
     // Enum
     var v18 = Status.Runing;
     if (v18 == Status.Runing) {
-      print("running");
+      debugPrint("running");
     }
 
     // func
-    print(add(1, 2, 3));
-    print(add1(z: 1, x: 1));
+    debugPrint(add(1, 2, 3));
+    debugPrint(add1(z: 1, x: 1));
     int add2(int x) {
       return x + 1;
     }
 
-    print(add2(1));
+    debugPrint(add2(1));
 
     Function add3(int x) {
       return (int x1) => x + x1;
     }
 
-    print(add3(1)(1));
+    debugPrint(add3(1)(1));
   }
 
   // case 6
   {
     num v19 = 1;
     int v20 = v19 as int;
-    print(v20);
-    print(v20 is int);
-    print(v20 is num);
-    print(v20 is! num);
-    print([true == true ? 'yes' : 'no', null ?? false]);
+    debugPrint(v20);
+    debugPrint(v20 is int);
+    debugPrint(v20 is num);
+    debugPrint(v20 is! num);
+    debugPrint([true == true ? 'yes' : 'no', null ?? false]);
 
     bool v21;
     v21 = true;
     v21 ??= false;
-    print(v21);
+    debugPrint(v21);
 
     var v22 = StringBuffer();
     v22..write("hello")..write("world");
-    print(v22);
+    debugPrint(v22);
 
     for (var i = 0; i < 0x9; i++) {
       if (i % 2 == 0) {
         continue;
       }
-      print(i);
+      debugPrint(i);
     }
     while (true) {
-      print(0);
+      debugPrint(0);
       break;
     }
     do {
-      print(0);
+      debugPrint(0);
       break;
     } while (true);
 
     var v23 = 2;
     switch (v23) {
       case 1:
-        print("case 1");
+        debugPrint("case 1");
         break;
       case 2:
-        print("case 2");
+        debugPrint("case 2");
         continue mark1;
       mark1:
       default:
-        print("not found");
+        debugPrint("not found");
     }
 
     try {
       throw OutOfMemoryError();
     } on OutOfMemoryError {
-      print("no Mem");
+      debugPrint("no Mem");
       // rethrow;
     } catch (e) {
-      print(e);
+      debugPrint(e);
     } finally {
-      print("finally");
+      debugPrint("finally");
     }
   }
 
   // case 7
   {
     var v24 = Point(1, 2);
-    print([v24.x, v24.y, v24.yes]);
+    debugPrint([v24.x, v24.y, v24.yes]);
     var v25 = Point.fromJson({"x": 1, "y": 2, "yes": 3});
-    print([v25.x, v25.y, v25.yes]);
+    debugPrint([v25.x, v25.y, v25.yes]);
 
     var v26 = People();
     v26.pName = "foobar";
-    print([
+    debugPrint([
       v26.pName,
       People.age,
     ]);
@@ -249,10 +261,10 @@ void main() async {
     var m1 = Map<String, int>();
     m1["foo"] = 1;
 
-    print([l1, s1, m1]);
+    debugPrint([l1, s1, m1]);
 
     k addCache<k, v>(k key, v val) {
-      print([key, val]);
+      debugPrint([key, val]);
       return key;
     }
 
@@ -267,7 +279,7 @@ void main() async {
   // case 10
   if (false) {
     // ref https://zhuanlan.zhihu.com/p/83781258
-    print("begin");
+    debugPrint("begin");
     Future<String> getNetworkData() {
       return Future<String>(() {
         sleep(Duration(seconds: 3));
@@ -277,71 +289,71 @@ void main() async {
     }
 
     var future = getNetworkData();
-    future.then((value) => print(value)).catchError((error) {
-      print(error);
+    future.then((value) => debugPrint(value)).catchError((error) {
+      debugPrint(error);
     });
-    print(future);
-    print("end");
+    debugPrint(future);
+    debugPrint("end");
   }
 
   // case 11
   if (false) {
     // ref https://zhuanlan.zhihu.com/p/83781258
     // await will return a Future immediately, then execute outer layer code
-    print("begin1");
+    debugPrint("begin1");
     Future<String> getNetworkData1() async {
       sleep(Duration(seconds: 3));
       return await "get data:" + "network data1";
     }
 
     Future<void> run1() async {
-      print("run1.begin");
-      print(await getNetworkData1());
-      print("run1.end");
+      debugPrint("run1.begin");
+      debugPrint(await getNetworkData1());
+      debugPrint("run1.end");
     }
 
     run1();
-    print("end1");
+    debugPrint("end1");
   }
 
   // case 12
   if (false) {
     Iterable<int> func1(int n) sync* {
-      print('begin');
+      debugPrint('begin');
       var k = 0;
       while (k < n) {
         yield k++; // step 2
       }
-      print('end');
+      debugPrint('end');
     }
 
     var it = func1(5).iterator;
     while (it.moveNext()) {
       // step 1
-      print(it.current); // step 3
+      debugPrint(it.current); // step 3
     }
   }
 
   // case 13
   if (false) {
     Stream<int> func1(int n) async* {
-      print('begin');
+      debugPrint('begin');
       var k = 0;
       while (k < n) {
         yield k++;
       }
-      print('end');
+      debugPrint('end');
     }
 
     if (false) {
       func1(5).listen((event) {
-        print(event);
+        debugPrint(event);
       });
     }
 
     var sub1 = func1(5).listen(null);
     sub1.onData((data) {
-      print(data);
+      debugPrint(data);
       // sub1.pause();
     });
   }
@@ -358,7 +370,7 @@ void main() async {
     var i1 = func1(5).iterator;
     while (i1.moveNext()) {
       // step 1
-      print(i1.current); // step 4
+      debugPrint(i1.current); // step 4
     }
   }
 
@@ -376,9 +388,9 @@ void main() async {
       if (msg is SendPort) {
         int n = 42;
         msg.send(n);
-        print("main Isolate: send int: $n");
+        debugPrint("main Isolate: send int: $n");
       } else {
-        print("main Isolate: recv int: $msg");
+        debugPrint("main Isolate: recv int: $msg");
       }
     });
   }
@@ -404,11 +416,11 @@ void newIsolateMain(SendPort mainSendPort) {
   var newPort = ReceivePort();
   mainSendPort.send(newPort.sendPort);
   newPort.listen((msg) async {
-    print("sub Isolate: recv msg: $msg");
+    debugPrint("sub Isolate: recv msg: $msg");
     if (msg is int) {
       final value = await slowPlusOne(msg);
       mainSendPort.send(value);
-      print("sub Isolate: send msg: $value");
+      debugPrint("sub Isolate: send msg: $value");
     }
   });
 }
@@ -418,7 +430,7 @@ Future<int> slowPlusOne(int n) =>
 
 class Foobar1 {
   call(String name) {
-    print("name is $name");
+    debugPrint("name is $name");
   }
 }
 
@@ -429,7 +441,7 @@ class Phone<T extends AndroidPhone> {
 
 abstract class bar {
   void msg() {
-    print("bar.func1");
+    debugPrint("bar.func1");
   }
 
   factory bar(int t) {
@@ -446,14 +458,14 @@ abstract class bar {
 class bar1 implements bar {
   @override
   void msg() {
-    print("bar1.func1");
+    debugPrint("bar1.func1");
   }
 }
 
 class bar2 implements bar {
   @override
   void msg() {
-    print("bar2.func1");
+    debugPrint("bar2.func1");
   }
 }
 
@@ -469,33 +481,33 @@ foo fooFactory(int t) {
 
 class foo {
   void msg() {
-    print("foo.msg");
+    debugPrint("foo.msg");
   }
 }
 
 class foo1 extends foo {
   @override
   void msg() {
-    print("foo1.msg");
+    debugPrint("foo1.msg");
   }
 }
 
 class foo2 extends foo {
   @override
   void msg() {
-    print("foo2.msg");
+    debugPrint("foo2.msg");
   }
 }
 
 class a {
   void func1() {
-    print("a.func1");
+    debugPrint("a.func1");
   }
 }
 
 class b {
   void func1() {
-    print("b.func1");
+    debugPrint("b.func1");
   }
 }
 
@@ -509,7 +521,7 @@ class AndroidPhonePlus extends AndroidPhone {
   AndroidPhonePlus(int num) : super(num);
   @override
   void startup() {
-    print("startup by sub class");
+    debugPrint("startup by sub class");
     return super.startup();
   }
 }
@@ -524,19 +536,19 @@ class AndroidPhone implements IPhone {
   AndroidPhone(this.number);
   @override
   void startup() {
-    print("startup");
+    debugPrint("startup");
   }
 
   @override
   void shutdown() {
-    print("shutdown");
+    debugPrint("shutdown");
   }
 }
 
 abstract class People1 {
   static String name = "foobar";
   void printName() {
-    print(name);
+    debugPrint(name);
   }
 }
 
@@ -545,14 +557,14 @@ class UsePeople1 extends People1 {}
 class UsePeople2 implements People1 {
   @override
   void printName() {
-    print('i am UsePeople2');
+    debugPrint('i am UsePeople2');
   }
 }
 
 class People {
   static int age = 22;
   static void showAge() {
-    print(age);
+    debugPrint(age);
   }
 
   // private member
